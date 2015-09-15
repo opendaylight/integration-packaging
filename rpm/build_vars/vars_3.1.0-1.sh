b7d46d9e2e62ba9194b8dd3bd682c0fd9462f613
Daniel Farrell <dfarrell@redhat.com>
2015-09-15 12:05:33 -0400

    Add ODL Lithium SR1 RPM build definition
    Add a build_vars file to define a Lithium SR1 RPM.
    
    Makes use of the new multi-RPM support added to build.sh and the ODL
    spec file to support 2.4, 3.0 and 3.1 builds at the same time.
    
    Change-Id: If4ad8563f4e9c09dfa3f6b246032dc2925824c3a
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ rpm/build_vars/vars_3.1.0-1.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+# Variables to define an OpenDaylight RPM build
+# `build.sh` reads these and passes them to `opendaylight.spec`
+# To define a new ODL RPM build, copy one of these and adjust as-needed
+
+# Directory used for caching build artifacts
+# Customizing this is useful for some offline builds
+cache_dir="/vagrant"
+
+# Version fields for the ODL release
+version_major=3
+version_minor=1
+
+# Include variable defaults that are common to many builds
+# NB: To use vars in default_vars.sh, define them before this source cmd
+#     Example: version_[major,minor,patch] is used for [odl,rpm]_version
+# NB: To override vars things defined in default_vars, define them after
+source $cache_dir/build_vars/default_vars.sh
+
+# Elemental codename for the ODL release, including SR if applicable
+codename="Lithium-SR1"
+
+# RPM version for the given ODL major.minor.patch
+rpm_release=1
