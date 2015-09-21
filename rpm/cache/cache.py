142c69b93dd90b0e600ab0190850f40fa6868989
Daniel Farrell <dfarrell@redhat.com>
2015-09-21 16:13:37 -0400

    Refactor RPM build to use YAML+Py+Jinja2 templates
    The previous approach used RPM macros, read in from .sh vars files and
    passed to the .spec file via rpmbuild, to customize the build per-RPM.
    
    This worked well, but turned out be drastically more complex on a remote
    build system like the CentOS CBS. Koji requires the macros to be defined
    in a painful way (via a second RPM to install the macro default file).
    
    This method reads build variables from a YAML config file, then uses
    Jinja2 templates to create static ODL .spec files for each RPM build.
    This moves the variable handling logic to Python, before the RPM's
    building logic is invoked, avoiding the need to define macros in Koji.
    
    Change-Id: Id12b32c14cab1df3d1580be9c77e1eca25113094
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ rpm/cache/cache.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""Read YAML description of RPM builds and cache the required artifacts."""
+
+import sys
+import os
+import urllib
+import tarfile
+from string import Template
+
+try:
+  import yaml
+except ImportError:
+  sys.stderr.write("ERROR: Are you using our Vagrant env?")
+  raise
+
+# Templates that can be specialized into artifact names/paths per-build
+odl_template = Template("distribution-karaf-0.$version_major."
+  "$version_minor-$codename.tar.gz")
+odl_url_template = Template("https://nexus.opendaylight.org/content/"
+  "groups/public/org/opendaylight/integration/distribution-karaf/0."
+  "$version_major.$version_minor-$codename/distribution-karaf-0."
+  "$version_major.$version_minor-$codename.tar.gz")
+unitfile_template = Template("opendaylight-$sysd_commit.service")
+unitfile_url_template = Template("https://git.opendaylight.org/gerrit/"
+  "gitweb?p=integration/packaging.git;a=blob_plain;f=rpm/unitfiles/"
+  "opendaylight.service;hb=$sysd_commit")
+unitfile_tb_template = Template("opendaylight-$sysd_commit.service.tar.gz")
+
+# Path to the directory that contains this file is assumed to be the cache dir
+cache_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Later paths are assumed to be relative to the cache dir
+os.chdir(cache_dir)
+
+# Load RPM build variables from a YAML config file
+with open(os.path.join(cache_dir, os.pardir, "build_vars.yaml")) as var_file:
+  build_vars = yaml.load(var_file)
+
+def cache_build(build):
+  """Cache the artifacts required for the given RPM build.
+
+  :param build: Description of an RPM build, typically from rpm_vars.yaml
+  :type build: dict
+
+  """
+  # Specialize a series of name/URL templates for the given build
+  odl_tarball = odl_template.substitute(build)
+  odl_tarball_url = odl_url_template.substitute(build)
+  unitfile = unitfile_template.substitute(build)
+  unitfile_url = unitfile_url_template.substitute(build)
+  unitfile_tarball = unitfile_tb_template.substitute(build)
+
+  # Cache appropriate version of OpenDaylight's release tarball
+  if not os.path.isfile(odl_tarball):
+    urllib.urlretrieve(odl_tarball_url, odl_tarball)
+    print("Cached: {}".format(odl_tarball))
+  else:
+    print("Already cached: {}".format(odl_tarball))
+
+  # Cache appropriate version of OpenDaylight's systemd unitfile as a tarball
+  if not os.path.isfile(unitfile_tarball):
+    urllib.urlretrieve(unitfile_url, unitfile)
+    with tarfile.open(unitfile_tarball, "w:gz") as tb:
+      tb.add(unitfile)
+    os.remove(unitfile)
+    print("Cached: {}".format(unitfile_tarball))
+  else:
+    print("Already cached: {}".format(unitfile_tarball))
+
+# If run as a script, cache artifacts required for all builds
+if __name__ == "__main__":
+  for build in build_vars["builds"]:
+    cache_build(build)
---
+++ rpm/specs/build_specs.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# Build RPM .spec files from YAML RPM config and a Jinja2 .spec template
+
+import os
+
+try:
+  import yaml
+  from jinja2 import Environment, FileSystemLoader
+except ImportError:
+  sys.stderr.write("ERROR: Are you using our Vagrant env?")
+  raise
+
+# Path to the directory that contains this file is assumed to be the spec dir
+spec_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Create the Jinja2 Environment
+env = Environment(loader=FileSystemLoader(spec_dir))
+
+# Load the OpenDaylight RPM .spec file template
+template = env.get_template("opendaylight.spec")
+
+# Load RPM build variables from a YAML config file
+with open(os.path.join(spec_dir, os.pardir, "build_vars.yaml")) as var_file:
+  rpm_vars = yaml.load(var_file)
+
+def build_spec(build):
+  """Build the RPM .spec file from a template for the given build description.
+
+  :param build: Description of an RPM build, typically from rpm_vars.yaml
+  :type build: dict
+
+  """
+  specfile_name = "opendaylight-{}.{}.{}-{}.spec".format(
+    build["version_major"],
+    build["version_minor"],
+    build["version_patch"],
+    build["rpm_release"])
+  specfile_path = os.path.join(spec_dir, specfile_name)
+  with open(specfile_path, "w") as specfile:
+    specfile.write(template.render(build))
+
+# If run as a script, build .spec files for all builds
+if __name__ == "__main__":
+  for build in rpm_vars["builds"]:
+    build_spec(build)
