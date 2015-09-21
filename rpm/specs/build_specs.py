3029824561e93f4490aef19d01b8bafc338d4bd4
Daniel Farrell <dfarrell@redhat.com>
2015-09-21 16:13:37 -0400

    Refactor RPM build to use YAML+Py+Jinja2 templates
    The previous approach used RPM macros, read in from .sh vars files and
    passed to the .spec file via rpmbuild, to customize the build per-RPM.
    
    This worked well, but turned out be drastically more complex on a remote
    build system like the CentOS CBS. Koji requires the macros to be defined
    in a painful way (via a second RPM to install the macro default file).
    
    This method reads build variables from a YAML config file, then uses
    Jinja2 templates to create static ODL .spec files for each RPM build.
    This moves the variable handling logic to Python, before the RPM's
    building logic is invoked, avoiding the need to define macros in Koji.
    
    Change-Id: Id12b32c14cab1df3d1580be9c77e1eca25113094
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ rpm/specs/build_specs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Build RPM .spec files from YAML RPM config and a Jinja2 .spec template
+
+from jinja2 import Environment, FileSystemLoader
+import yaml
+import os
+
+# Path to OpenDaylight .spec file template dir
+spec_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Create the Jinja2 Environment
+env = Environment(loader=FileSystemLoader(spec_dir))
+
+# Load the OpenDaylight RPM .spec file template
+template = env.get_template("opendaylight.spec")
+
+# Load RPM build variables from a YAML config file
+with open(os.path.join(spec_dir, "spec_vars.yaml")) as var_file:
+    rpm_vars = yaml.load(var_file)
+
+# Use the RPM .spec template to build a .spec file for each build vars set
+for build in rpm_vars["builds"]:
+    specfile_name = "opendaylight_{}.{}.{}-{}.spec".format(
+      build["version_major"],
+      build["version_minor"],
+      build["version_patch"],
+      build["rpm_release"])
+    specfile_path = os.path.join(spec_dir, specfile_name)
+    with open(specfile_path, "w") as specfile:
+        specfile.write(template.render(build))
