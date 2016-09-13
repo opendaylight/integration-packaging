7d59aadaa88763ae6ff504a895a5753fe7314f77
Akshita Jha <zenith158@gmail.com>
2016-09-13 20:37:59 +0530

    Adds the feature to build ODL's .deb package from local tarball
    The file `cache/cache.py` caches the tarball required for the given
    debian build. `build.py` now uses a local tar.gz file, if it exists;
    otherwise `cache/cache.py` downloads the appropriate version of
    OpenDaylight's release tarball.
    
    This commit further removes all old debian files created during
    previous debian packaging.
    
    It also removes executable permission from `build_vars.yaml`.
    
    Change-Id: Ida69d19b34751bd7f221b8756348b9a77c3decd0
    Signed-off-by: Akshita Jha <zenith158@gmail.com>

--- deb/build.py
+++ deb/build.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import shutil
 import argparse
 import subprocess
 from string import Template
@@ -14,11 +15,15 @@ except ImportError:
     sys.stderr.write("Else, install the Python libs it installs.\n")
     raise
 
+import cache.cache as cache
 import templates.build_debianfiles as build_debfiles
 
 # Common paths used in this script
 # This file is assumed to be in the root of the .deb build logic's dir structure
 project_root = os.path.dirname(os.path.abspath(__file__))
+
+# Cache directory for OpenDaylight's release tarball
+cache_dir = os.path.join(project_root, "cache")
 
 # Debian templates directory
 templates_dir = os.path.join(project_root, "templates")
@@ -26,6 +31,8 @@ templates_dir = os.path.join(project_root, "templates")
 # Specialized opendaylight directory for each build
 odl_dir_template = Template("opendaylight/opendaylight-$version_major.$version_minor."
                             "$version_patch-$pkg_version/")
+odl_deb_template = Template("opendaylight/opendaylight_$version_major.$version_minor."
+                            "$version_patch-${pkg_version}_all.deb")
 
 
 def build_deb(build):
@@ -35,11 +42,19 @@ def build_deb(build):
     :type build: dict
 
     """
+    # Specialize a series of name templates for the given build
     odl_dir_name = odl_dir_template.substitute(build)
     odl_dir_path = os.path.join(templates_dir, os.pardir, odl_dir_name)
+    odl_deb = odl_deb_template.substitute(build)
 
     # Call helper script to build the required debian files
     build_debfiles.build_debfiles(build)
+
+    # Call a helper function to cache the artifacts required for each build
+    odl_tarball_path = cache.cache_build(build)
+
+    # Move ODL's tarball to the specialized OpenDaylight directory
+    shutil.copy(odl_tarball_path, odl_dir_path)
 
     # Build debian package
     os.chdir(odl_dir_path)
@@ -50,6 +65,9 @@ def build_deb(build):
     subprocess.call(["sudo mk-build-deps -i " + control_file_path], shell=True)
 
     os.chdir(project_root)
+
+    # Copy the .debs from their output dir to the cache dir
+    shutil.copy(odl_deb, cache_dir)
 
 
 # When run as a script, accept a set of builds and execute them
