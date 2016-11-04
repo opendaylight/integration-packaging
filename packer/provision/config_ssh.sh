e976a37be21994eb54a978cbd1a87ffc78e596a4
Daniel Farrell <dfarrell@redhat.com>
2016-11-04 01:28:41 -0400

    Fix Packer SSH config to work with both F24/Cent7
    Change-Id: I7a36b00f2230afb24307b19cb8db4dc316731f76
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- packer/provision/config_ssh.sh
+++ packer/provision/config_ssh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Recent versions of OpenSSH don't support ssh-dss as an auth protocol
-# ODL seems offers ssh-dss by default. To SSH to the Karaf shell, tell
+# OpenSSH >=7.0 don't support ssh-dss as an auth protocol, which ODL
+# seems to offer by default. To SSH to the Karaf shell, tell
 # SSH to accept ssh-dss. Alternatively, tell SSH to accept ssh-dss via
 # the HostKeyAlgorithms option at invocation.
 #   ssh -p 8101 -oHostKeyAlgorithms=+ssh-dss karaf@localhost
@@ -11,4 +11,4 @@
 set -ex
 
 # Accept ssh-dss as an SSH algorithm by appending config to end of file
-sudo sed -i -e "\$aHostKeyAlgorithms=+ssh-dss" /etc/ssh/ssh_config
+sudo sed -i -e "\$aHostKeyAlgorithms ssh-rsa" /etc/ssh/ssh_config
