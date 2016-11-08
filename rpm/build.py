c7ef14e0f151ff0e74cfd235e581a6388ab21b7b
Girish Sukhatankar <girish.sukhatankar@colorado.edu>
2016-11-07 23:38:28 -0700

    Extract version logic from URL in RPM Build Script
    Modified the function extract_version() to account
    for fixes.
    
    Add unit tests for version extraction logic, doc.
    
    Change-Id: Id765c2cca9ad10c88efb08c6ac2bb98b1b0ef4c8
    Signed-off-by: Girish Sukhatankar <girish.sukhatankar@colorado.edu>
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- rpm/build.py
+++ rpm/build.py
@@ -6,7 +6,9 @@ import sys
 import argparse
 import shutil
 import subprocess
+import re
 from string import Template
+from datetime import datetime
 
 try:
     import yaml
@@ -39,6 +41,61 @@ rpm_template = Template("opendaylight-$version_major.$version_minor."
                         "$version_patch-$rpm_release.el7.noarch.rpm")
 srpm_template = Template("opendaylight-$version_major.$version_minor."
                          "$version_patch-$rpm_release.el7.src.rpm")
+
+
+def extract_version(url):
+    """Determine ODL version information from the ODL tarball build URL.
+
+    :param url: URL of the ODL tarball build for building RPMs.
+    :type url: string
+    returns: Dict of ODL major version, minor version, patch version,
+              RPM relase version and elemental codename.
+
+    """
+    # Substitute the part of the build URL not required with white space
+    date_url = re.sub('distribution-karaf-.*\.tar\.gz$', '', url)
+    # Set date_url as an environmental variable for it to be used in a subprocess
+    os.environ["date_url"] = date_url
+    # Extract ODL artifact's date by scraping data from the build URL
+    odl_date = subprocess.Popen("curl -s $date_url | grep tar.gz -A1 | tail -n1 | sed \"s/<td>//g\""
+                                "| sed \"s/\\n//g\" | awk '{print $3,$2,$6}' ", shell=True,
+                                stdout=subprocess.PIPE, stdin=subprocess.PIPE).stdout.read() \
+               .rstrip().strip("</td>")
+    date = datetime.strptime(odl_date, "%d %b %Y").strftime('%Y%m%d')
+
+    if "autorelease" in url:
+        # Search the ODL autorelease build URL to match the Build ID that follows "autorelease-".
+        # eg: https://nexus.opendaylight.org/content/repositories/autorelease-1533/org/
+        #     opendaylight/integration/distribution-karaf/0.4.4-Beryllium-SR4/
+        # build_id = 1533
+        build_id = re.search(r'\/(autorelease)-([0-9]+)\/', url).group(2)
+        rpm_release = "0.1." + date + "rel" + build_id
+    elif "snapshot" in url:
+        # Search the ODL snapshot build URL to match the Build ID that preceeds ".tar.gz".
+        # eg: https://nexus.opendaylight.org/content/repositories/opendaylight.snapshot/org/
+        #     opendaylight/integration/distribution-karaf/0.6.0-SNAPSHOT/
+        #     distribution-karaf-0.6.0-20161201.031047-2242.tar.gz
+        # build_id = 2242
+        build_id = re.search(r'-([0-9]+)\.(tar\.gz)', url).group(1)
+        rpm_release = "0.1." + date + "snap" + build_id
+    elif "public" or "opendaylight.release" in url:
+        rpm_release = "1"
+    else:
+        raise ValueError("Unrecognized URL {}".format(url))
+
+    version = {}
+    # Search the ODL build URL to match 0.major.minor-codename-SR and extarct version information.
+    # eg: release: https://nexus.opendaylight.org/content/repositories/public/org/opendaylight/
+    #     integration/distribution-karaf/0.3.3-Lithium-SR3/
+    #     distribution-karaf-0.3.3-Lithium-SR3.tar.gz
+    #     match: 0.3.3-Lithium-SR3
+    odl_version = re.search(r'\/(\d)\.(\d)\.(\d).(.*)\/', url)
+    version["version_major"] = odl_version.group(2)
+    version["version_minor"] = odl_version.group(3)
+    version["version_patch"] = "0"
+    version["rpm_release"] = rpm_release
+    version["codename"] = odl_version.group(4)
+    return version
 
 
 def build_rpm(build):
