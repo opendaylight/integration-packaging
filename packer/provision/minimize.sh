c10f24f1183796a6823dbaec6aed6ef8fb9725f9
Daniel Farrell <dfarrell@redhat.com>
2017-01-12 19:23:06 -0500

    Fixs in Packer cleanup/minimize scripts
    * Need root, use sudo.
    * Don't do yum remove of packages that aren't installed
    * Fix shebang
    
    Change-Id: I883d32898e60333530e2e976710dbd73399aba4b
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- packer/provision/minimize.sh
+++ packer/provision/minimize.sh
@@ -1,7 +1,19 @@
-#!bin/sh -eux
+#!/usr/bin/env bash
 
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+set -ex
+
+# This doesn't seem to reduce the size of the box, and may break
+# Docker builds. See comments on git.opendaylight.org/gerrit/#/c/50382.
+
+# Write 0s in free space until full (full->exit 1, force 0)
+#sudo dd if=/dev/zero of=/EMPTY bs=1M || true
+
+# Remove pointer to all the 0-space
+#sudo rm -f /EMPTY
+
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
-sync
+#sudo sync
