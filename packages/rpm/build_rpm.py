362f64185ce65d3b5cb24f211fe4ef17dce9a3f8
Alok Anand <alok4nand@gmail.com>
2017-06-22 11:23:44 +0530

    Refactor package building logic for RPMs/Debs
    The refactoring creates a common entry point for building both
    rpms and debs. The common `build.py` script now takes an
    additional flag `--rpm|--deb` and branches out accordingly.
    
    Example usage:
    ./build.py --rpm --download_url <url>...
    ./build.py --deb --build-latest-snap --major <major_version>...
    
    The logic for creating the release packages according to stored
    build_vars.yaml files have been removed.
    
    Change-Id: I888ad5f7f7eecfa9db8debb508b696b560d220d6
    Signed-off-by: Alok Anand <alok4nand@gmail.com>

---
+++ packages/rpm/build_rpm.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+##############################################################################
+# Copyright (c) 2016 Daniel Farrell and Others.  All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+"""Build OpenDaylight's RPMs using build configs """
+
+import os
+import shutil
+from string import Template
+import subprocess
+
+import cache.cache as cache
+import specs.build_specs as build_specs
+
+# Common paths used in this script
+# This file is assumed to be in the root of the RPM build logic's dir structure
+rpm_root = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(rpm_root)
+cache_dir = os.path.join(project_root, "cache")
+specs_dir = os.path.join(rpm_root, "specs")
+rpmbuild_dir = os.path.join(os.path.expanduser("~"), "rpmbuild")
+src_in_dir = os.path.join(rpmbuild_dir, "SOURCES")
+spec_in_dir = os.path.join(rpmbuild_dir, "SPECS")
+srpm_out_dir = os.path.join(rpmbuild_dir, "SRPMS")
+rpm_out_dir = os.path.join(rpmbuild_dir, "RPMS", "noarch")
+
+# Templates that can be specialized into common artifact names per-build
+specfile_template = Template("opendaylight-$version_major.$version_minor."
+                             "$version_patch-$pkg_version.spec")
+rpm_template = Template("opendaylight-$version_major.$version_minor."
+                        "$version_patch-$pkg_version.el7.noarch.rpm")
+srpm_template = Template("opendaylight-$version_major.$version_minor."
+                         "$version_patch-$pkg_version.el7.src.rpm")
+
+
+def build_rpm(build):
+    """Build the RPMs described by the given build description
+
+    :param build: Description of an RPM build
+    :type build: dict
+
+    """
+    # Specialize a series of name templates for the given build
+    odl_rpm = rpm_template.substitute(build)
+    odl_srpm = srpm_template.substitute(build)
+    odl_specfile = specfile_template.substitute(build)
+
+    # After building strings from the name templates, build their full path
+    # odl_tarball_path = os.path.join(cache_dir, odl_tarball)
+    specfile_path = os.path.join(specs_dir, odl_specfile)
+    spec_in_path = os.path.join(spec_in_dir, odl_specfile)
+    rpm_out_path = os.path.join(rpm_out_dir, odl_rpm)
+    srpm_out_path = os.path.join(srpm_out_dir, odl_srpm)
+
+    # Call a helper function to cache the artifacts required for each build
+    odl_tarball_path = cache.cache_odl_tarball(build)
+    unitfile_tarball_path = cache.cache_odl_unitfile(build)
+
+    # Call helper script to build the required RPM .spec files
+    build_specs.build_spec(build)
+
+    # Clean up old rpmbuild dir structure if it exists
+    if os.path.isdir(rpmbuild_dir):
+        shutil.rmtree(rpmbuild_dir)
+
+    # Create rpmbuild dir structure
+    subprocess.call("rpmdev-setuptree")
+
+    # Move unitfile, tarball and specfile to correct rpmbuild dirs
+    shutil.copy(odl_tarball_path, src_in_dir)
+    shutil.copy(unitfile_tarball_path, src_in_dir)
+    shutil.copy(specfile_path, spec_in_dir)
+
+    # Call rpmbuild, build both SRPMs/RPMs
+    subprocess.call(["rpmbuild", "-ba", spec_in_path])
+
+    # Copy the RPMs/SRPMs from their output dir to the cache dir
+    shutil.copy(rpm_out_path, cache_dir)
+    shutil.copy(srpm_out_path, cache_dir)
