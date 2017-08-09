c1599d1cadc8418a34ccfe297a8706b8895c3654
Alok Anand <alok4nand@gmail.com>
2017-08-09 16:09:14 +0530

    Remove YAML logic from RPM's spec & cache
    As the YAML logic has been removed from building RPM packages these
    functionalities are no more relevant.
    
    Change-Id: I67c393bd02e02d7248acb022fc16363c15f6bab6
    Signed-off-by: Alok Anand <alok4nand@gmail.com>

--- rpm/specs/build_specs.py
+++ rpm/specs/build_specs.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-"""Build RPM .spec files from YAML RPM config and a Jinja2 .spec template."""
+"""Build RPM .spec files from build description and a Jinja2 .spec template."""
 
 import os
 from string import Template
 import sys
 
 try:
-    import yaml
     from jinja2 import Environment, FileSystemLoader
 except ImportError:
     sys.stderr.write("We recommend using our included Vagrant env.\n")
@@ -30,7 +29,7 @@ specfile_template = Template("opendaylight-$version_major.$version_minor."
 def build_spec(build):
     """Build the RPM .spec file from a template for the given build description.
 
-    :param build: Description of an RPM build, typically from build_vars.yaml
+    :param build: Description of an RPM build
     :type build: dict
 
     """
@@ -38,13 +37,3 @@ def build_spec(build):
     specfile_path = os.path.join(spec_dir, specfile_name)
     with open(specfile_path, "w") as specfile:
         specfile.write(template.render(build))
-
-
-# If run as a script, build .spec files for all builds
-if __name__ == "__main__":
-    # Load RPM build variables from a YAML config file
-    with open(os.path.join(spec_dir, os.pardir, "build_vars.yaml")) as var_fd:
-        build_vars = yaml.load(var_fd)
-
-    for build in build_vars["builds"]:
-        build_spec(build)
