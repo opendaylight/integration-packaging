a5d90c715f463d20174e2780a2ac5026204c6ea1
Daniel Farrell <dfarrell@redhat.com>
2017-08-09 15:59:15 -0400

    Update packages at end of configuration
    We already do a yum/dnf update at the start of the provisioning in the
    Kickstart scripts, but by the end of the process there are additional
    packages that should be updated.
    
    Change-Id: I48d38aac680805894a9a21850206d57731a6bd38
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ packer/provision/update_centos.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+sudo yum update -y
